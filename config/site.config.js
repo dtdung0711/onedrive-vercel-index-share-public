9f839c1d2956d03a1a9f8c57070240a5461f49c1
vitocorleone <111355843+dtdung0711@users.noreply.github.com>
2023-07-28 09:17:26 +0700

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -52,8 +52,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      name: 'Facebook',
+      link: 'https://www.facebook.com/profile.php?id=100090370753559',
     },
   ],
 
